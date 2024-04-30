8cf7f9e00a8449263dc9b1c00e4459d7bf3df1e6
sal <50084570+playboiix@users.noreply.github.com>
2024-04-30 11:41:34 -0400

    asd

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
     <head>
         <title>Spidey Station</title>
         <link rel="icon" type="image/x-icon" href="spidey.ico">
-        <?php
-        echo "test test yurr</br>";
-        echo date('Y-m-d H:i:s');
-        ?>
     </head>
+
+    <body>
+        <h1> YURR TEST </h1>
+        <p> This is a PARAGRAPH </p>
 </html>
