6e2a23f22ffda07214150b106d0564439b96c816
sal <50084570+playboiix@users.noreply.github.com>
2024-04-30 11:38:18 -0400

    icon

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <html>
     <head>
         <title>Spidey Station</title>
+        <link rel="icon" type="image/x-icon" href="/images/spidey.ico">
         <?php
         echo "test test yurr</br>";
         echo date('Y-m-d H:i:s');
         ?>
+
     </head>
 </html>
