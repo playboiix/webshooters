43452b9336d4b0ffbee402c804ead87c7bbb73f6
sal <50084570+playboiix@users.noreply.github.com>
2024-04-30 22:50:29 -0400

    asd

--- index.php
+++ index.php
@@ -9,6 +9,6 @@
       <h1> Your Friendly Neighborhood Spider-Man! </h1>
       <p> HEHE BOI </p>
       <div class="box"> da video goes here </div>
-      <button align="center"> Where is Spidey?? </button>
+      <button> Where is Spidey?? </button>
    </body>
 </html>
