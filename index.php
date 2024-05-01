14b80c28608c90b6a4d798802ffc030307322bb6
sal <50084570+playboiix@users.noreply.github.com>
2024-04-30 23:13:43 -0400

    asd

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
    <body>
       <h1>Web-Shooters Monitor</h1>
-      <p>Here comes our friendly neighborhood Spider-Man!</p>
+      <p>Here comes our Friendly Neighborhood Spider-Man!</p>
       <div class="video"> da video goes here </div>
    </body>
 </html>
